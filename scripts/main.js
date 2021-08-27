e45844cf67ac911e2142f33d312ba9b08c8f60b4
Igor Boitsev <igor.boitsev@gmail.com>
2021-08-27 14:25:49 +0300

    floor layout

--- scripts/main.js
+++ scripts/main.js
@@ -4,9 +4,13 @@ const homeLevels = document.querySelector('.home-levels'),
       levels = document.querySelectorAll('.level'),
       selectorUp = document.querySelector('.selector-up'),
       selectorDown = document.querySelector('.selector-down'),
-      selector = document.querySelector('.selector');
+      selector = document.querySelector('.selector'),
+      buttonPrimary = document.querySelector('.button-primary'),
+      modalFlats = document.querySelector('.modal-flats'),
+      flatsList = document.querySelector('.flats-list'),
+      closeBtn = document.querySelector('.close-btn');
 
-//
+// Функция смены подсветки этажа при нажатии на стрелочки
 const changeFloor = (direction) => {
 
   // Текущий,предыдущий и следуующий номера этажей
@@ -51,7 +55,7 @@ const changeFloor = (direction) => {
 
 }
 
-// 
+// Функция смены номера этажа при нажатии на стрелочки
 const checkFloorNumber = (direction) => {
 
   // Текущий,предыдущий и следующий номера этажей
@@ -61,11 +65,12 @@ const checkFloorNumber = (direction) => {
 
   switch (direction) {
     case 'up':
-      // 
       if (currenLlevelNumber == 18 || (currenLlevelNumber == 17 && upperLevelNumber == 18)) {
         selectorUp.style.display = 'none';
         selector.style.marginTop = '26px';
       } else {
+        selectorDown.style.display = 'block';
+        selector.style.marginBottom = '0px';
         selectorUp.style.display = 'block';
         selector.style.marginTop = '0px';
       }
@@ -75,8 +80,10 @@ const checkFloorNumber = (direction) => {
       //
       if (currenLlevelNumber == 2 || (currenLlevelNumber == 3 && lowerLevelNumber == 2)) {
         selectorDown.style.display = 'none';
-        selector.style.marginBottom = '26px';
+        selector.style.marginBottom = '27px';
       } else {
+        selectorUp.style.display = 'block';
+        selector.style.marginTop = '0px';
         selectorDown.style.display = 'block';
         selector.style.marginBottom = '0px';
       }
@@ -85,6 +92,7 @@ const checkFloorNumber = (direction) => {
 
 }
 
+// Смена подсветки при наведении мышкой на этаж
 levels.forEach(level => {
   level.addEventListener('mouseover', e => {
     // Скрытие выделенного этажа
@@ -93,16 +101,144 @@ levels.forEach(level => {
     e.target.classList.add('is-active');
     // Изменение номера этажа
     selector.textContent = e.target.dataset.floor;
+
+    if (e.target.dataset.floor == 18) {
+      selectorUp.style.display = 'none';
+      selector.style.marginTop = '26px';
+    } else {
+      selectorUp.style.display = 'block';
+      selector.style.marginTop = '0px';
+    }
+
+    if (e.target.dataset.floor == 2) {
+      selectorDown.style.display = 'none';
+      selector.style.marginBottom = '27px';
+    } else {
+      selectorDown.style.display = 'block';
+      selector.style.marginBottom = '0px';
+    }
   })
 })
 
+// Функия открытия модульного окна
+const modalFlatsOpen = (floorNumber) => {
+  const flatsLayoutHeader = modalFlats.querySelector('.flats-layout-header');
+  flatsLayoutHeader.textContent = `Этаж ${floorNumber}`;
+  modalFlats.style.display = 'flex';
+}
+
+// Функция получения данных о квартирах на этаже
+const getFloorInfo = async () => {
+  let floorInfo = await fetch('./floor-db.json')
+                          .then(response => response.json())
+
+  return floorInfo;
+}
+
+// Создание списка с описанием квартирами для модульного окна
+const flatsListRender = async (floorNumber) => {
+  let floor = '';
+  // let flatDescription = '';
+
+  await getFloorInfo()  
+    .then(floorList => {
+      floor = floorList[+floorNumber - 2];
+    });
+
+  floor.flatsList.forEach(elem => {
+    const flat = document.createElement('li');
+    flat.classList.add('flats-list-item');
+    if (elem.isSold)  flat.classList.add('sold');
+    flat.dataset.number = elem.number;
+    flat.textContent = `${elem.rooms ? elem.rooms + '-х комн. квартира' : 'студия'}, ${elem.square} кв. м`;
+
+    flatsList.append(flat);
+  })
+
+  // const flats = document.querySelectorAll('.flat'),
+  // flatsListItems = document.querySelectorAll('.flats-list-item');
+
+  // // Подсветка квартиры на плане при наведении на список квартир
+  // flatsListItems.forEach(item => {
+  //   item.addEventListener('mouseover', e => {
+
+  //     flats.forEach(flat => {
+  //       if (flat.dataset.number.substr(1) == e.target.dataset.number.substr(1)) {
+  //         flat.classList.add('is-active')
+  //       } else {
+  //         flat.classList.remove('is-active')
+  //       }
+  //     });
+  //   })
+  // })
+  const flats = document.querySelectorAll('.flat'),
+        flatsListItems = document.querySelectorAll('.flats-list-item');
+  // console.log(flatsListItems);
+  
+  // Подсветка квартиры на плане при наведении на список квартир
+  flatsListItems.forEach(item => {
+    item.addEventListener('mouseover', e => {
+      // console.log(1);
+      flats.forEach(flat => {
+        if (flat.dataset.number.substr(1) == e.target.dataset.number.substr(1)) {
+          flat.classList.add('is-active')
+        } else {
+          flat.classList.remove('is-active')
+        }
+      });
+    })
+  })
+  // Сброс подсветки квартиры, если не наведен курсор на список квартир
+  flatsListItems.forEach(item => {
+    item.addEventListener('mouseout', () => {
+      flats.forEach(flat => flat.classList.remove('is-active'))
+    })
+  })
+  // Подсветка описания квартиры при наведении на квартиру на плане
+  flats.forEach(flat => {
+    flat.addEventListener('mouseover', e => {
+      flatsListItems.forEach(item => {
+        if (e.target.dataset.number.substr(1) == item.dataset.number.substr(1)) {
+          item.classList.add('is-active')
+        } else {
+          item.classList.remove('is-active')
+        }
+      })
+    })
+  })
+  // Сброс подсветки квартиры
+  flats.forEach(flat => {
+    flat.addEventListener('mouseout', () => {
+      flatsListItems.forEach(item => item.classList.remove('is-active'))
+    })
+  })
+
+}
+
 // Этажом выше
 selectorUp.addEventListener('click', () => {
-  // checkFloorNumber('up');
+  checkFloorNumber('up');
   changeFloor('up');
 });
 // Этажом ниже
 selectorDown.addEventListener('click', () => {
-  // checkFloorNumber('down');
+  checkFloorNumber('down');
   changeFloor('down');
+})
+// Открытие модульного окна
+levels.forEach(level => {
+  level.addEventListener('click', e => {
+    flatsListRender(e.target.dataset.floor);
+    modalFlatsOpen(e.target.dataset.floor);
+  })
+
+
+})
+buttonPrimary.addEventListener('click', () => {
+  flatsListRender(selector.textContent);
+  modalFlatsOpen(selector.textContent);
+})
+// Закрытие модульного окна
+closeBtn.addEventListener('click', () => {
+  modalFlats.style.display = 'none';
 })
